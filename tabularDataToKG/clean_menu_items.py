46b88c947a18bbe9a6f36014d4aec3287cc67053
Tom Fishwick <fishwickt@gmail.com>
2023-03-04 18:16:07 +0000

    started sorting through pizzas

--- tabularDataToKG/clean_menu_items.py
+++ tabularDataToKG/clean_menu_items.py
@@ -5,6 +5,29 @@ import os
 from pizza_kg.data import get_org_data, OrgDataFiles
 
 
+def find_pizza(org: str, pizza_dict: dict) -> str:
+    """This looks for the pizza in the dictionary
+
+    Parameters
+    ----------
+    org: str
+        The original string
+
+    pizza_dict: dict
+        The dictionary to look in
+
+    Returns
+    -------
+    str
+        The matching value in the dictionary"""
+    if org.lower() == 'pizza':
+        return 'pizza'
+    for key, value in pizza_dict.items():
+        if key.lower() in org.lower():
+            return value
+    return ''
+
+
 def main() -> pd.DataFrame:
     """This is to get the cleaned menu items
 
@@ -33,13 +56,21 @@ def main() -> pd.DataFrame:
     replace_list = [
         ('ampcomma', ','),
         (' and ', ', '),
-        ('ampquot', '"')
+        ('ampquot', '"'),
+        ('ampamp', '&')
     ]
     for org_str, replace_str in replace_list:
         ret_df['menusDescription'] = ret_df['menusDescription']\
             .str.replace(org_str, replace_str)
         ret_df['menusName'] = ret_df['menusName']\
             .str.replace(org_str, replace_str)
+
+    # load the map of pizzas
+    pizza_df = pd.read_excel(os.path.join('tabularDataToKG', 'pizzaMapping.xlsx'), sheet_name='Sheet1', index_col='org')
+    print(pizza_df)
+    pizza_dict = pizza_df.to_dict()
+    print(pizza_dict['MapTo'])
+    ret_df['menuItem'] = ret_df['menusName'].apply(find_pizza, args=(pizza_dict['MapTo'],))
     return ret_df
 
 
