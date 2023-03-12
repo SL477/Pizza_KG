b5da3ca010edfcd1b79ad6c7f3d3c7621584b46a
Tom Fishwick <fishwickt@gmail.com>
2023-03-12 20:00:41 +0000

    started getting bianca restaurants

--- sparqlAndReasoning/pizza_bianca_restaurants.py
+++ sparqlAndReasoning/pizza_bianca_restaurants.py
@@ -11,15 +11,82 @@ def main():
     g.parse(os.path.join('sparqlAndReasoning', 'ontology_ttl.ttl'),
             format='ttl')
     query = """
-    SELECT DISTINCT ?restaurant
+    SELECT DISTINCT 
+        ?restaurant
+        ?restaurantLabel
+        ?restaurantComment
+        ?cityLabel
+        ?restaurantLongitude
+        ?restaurantLatitude
+        ?restaurantDateAdded
+        (group_concat(?restaurantDateUpdated) AS ?dateUpdated)
+        ?restaurantID
+        ?restaurantPostCode
+        ?restaurantCurrency
+        ?restaurantPriceMax
+        ?restaurantPriceMin
+        (group_concat(?primaryCategory) AS ?primaryCategories)
+        ?restaurantAddress
+        ?restaurantName
+        ?keys
+        (GROUP_CONCAT(?category) AS ?categories)
     WHERE {
-        ?restaurant rdf:type tef:country
-    } LIMIT 100
+        ?restaurant rdf:type tef:restaurant .
+        ?restaurant tef:hasMenuItem ?item .
+        ?item rdfs:label ?itemLabel .
+        ?restaurant rdfs:label ?restaurantLabel .
+        ?restaurant rdfs:comment ?restaurantComment .
+        ?restaurant tef:restaurantInCity ?city .
+        ?city rdfs:label ?cityLabel .
+        ?restaurant tef:longitude ?restaurantLongitude .
+        ?restaurant tef:latitude ?restaurantLatitude .
+        ?restaurant tef:dateAdded ?restaurantDateAdded .
+        ?restaurant tef:dateUpdated ?restaurantDateUpdated .
+        ?restaurant tef:priceRangeCurrency ?restaurantCurrency .
+        ?restaurant tef:id ?restaurantID .
+        ?restaurant tef:priceRangeMax ?restaurantPriceMax .
+        ?restaurant tef:priceRangeMin ?restaurantPriceMin .
+        ?restaurant tef:primaryCategories ?primaryCategory .
+        ?restaurant tef:address ?restaurantAddress .
+        ?restaurant tef:restaurantName ?restaurantName .
+        ?restaurant tef:keys ?keys .
+        ?restaurant tef:category ?category .
+        OPTIONAL {
+            ?restaurant tef:postalCode ?restaurantPostCode .
+        }
+        FILTER ( regex(lcase(str(?itemLabel)), "bianca") || regex(lcase(str(?itemLabel)), "white"))
+    }
+    GROUP BY ?restaurant
+        ?restaurantLabel
+        ?restaurantComment
+        ?cityLabel
+        ?restaurantLongitude
+        ?restaurantLatitude
+        ?restaurantDateAdded
+        ?restaurantID
+        ?restaurantPostCode
+        ?restaurantCurrency
+        ?restaurantPriceMax
+        ?restaurantPriceMin
+        ?restaurantAddress
+        ?restaurantName
+        ?keys
+    LIMIT 100
     """
 
     res = g.query(query)
+    cnt = 0
     for row in res:
-        print(row)
+        print(row.restaurant, row.restaurantLabel, row.restaurantComment,
+              row.cityLabel, row.restaurantLongitude,
+              row.restaurantLatitude, row.restaurantDateAdded,
+              row.dateUpdated, row.restaurantID, row.restaurantPostCode,
+              row.restaurantCurrency, row.restaurantPriceMax,
+              row.restaurantPriceMin, row.primaryCategories,
+              row.restaurantAddress, row.restaurantName, row.keys,
+              row.categories)
+        cnt += 1
+    print(f"Number of rows: {cnt}")
 
 
 if __name__ == '__main__':
